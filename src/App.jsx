8a620fe7a5133dab30bc3c6dd6bbedd8a68ae895
luisnava6667 <luisnavarro19890603@gmail.com>
2022-08-22 20:08:40 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import Header from "./components/Header";
+import ListadoPaciente from "./components/ListadoPaciente";
+import Formulario from "./components/Formulario";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [pacientes, setPacientes] = useState(JSON.parse(localStorage.getItem('pacientes')) ?? []);
+  const [paciente, setPaciente] = useState({});
+
+  
+  useEffect(() => {
+    localStorage.setItem("pacientes", JSON.stringify(pacientes));
+  }, [pacientes]);
+
+  const eliminarPaciente = (id) => {
+    const pacientesActualizado = pacientes.filter(
+      (paciente) => paciente.id !== id
+    );
+    setPacientes(pacientesActualizado);
+  };
+  return (
+    <div className="container mx-auto mt-20">
+      <Header />
+      <div className="mt-12 md:flex">
+        <Formulario
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
+        />
+        <ListadoPaciente
+          pacientes={pacientes}
+          setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
